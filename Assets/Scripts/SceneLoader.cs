05365de9f2624a130afb375f0517e2777ab79d90
B1g_B0$$ <grand0077@inbox.ru>
2026-02-06 12:49:31 +0300

    Настройка адрассаблов

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,33 +1,104 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadLevelScene(int index)
+    [SerializeField] private AssetReference[] _sceneReferences;
+    [SerializeField] private AssetReference _currentSceneRef;
+    
+    private AsyncOperationHandle<SceneInstance> _currentSceneHandle;
+    private List<string> _sceneAddresses = new ();
+    
+    private void Start()
     {
-        if (index >= 0 && index <= SceneManager.sceneCountInBuildSettings) 
-            SceneManager.LoadScene(index, LoadSceneMode.Single);
+        InitializeSceneAddresses();
     }
-
-    public void LoadNextScene()
+    
+    private void InitializeSceneAddresses()
     {
-        int sceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-        if (SceneManager.sceneCountInBuildSettings >= sceneIndex)
-            SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+        _sceneAddresses.Clear();
+        foreach (var sceneRef in _sceneReferences)
+        {
+            _sceneAddresses.Add(sceneRef.AssetGUID);
+        }
     }
-
-    public void LoadPreviousScene()
+    
+    public async void LoadLevelScene(int index)
     {
-        int sceneIndex = SceneManager.GetActiveScene().buildIndex - 1;
-        if (sceneIndex > 0)
-            SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+        if (index >= 0 && index < _sceneAddresses.Count)
+        {
+            await LoadSceneByAddress(_sceneAddresses[index]);
+        }
+        else
+        {
+            Debug.LogError($"Scene index {index} is out of range!");
+        }
     }
-
-    public void ReloadScene()
+    
+    public async void LoadNextScene()
     {
-        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+        int currentIndex = GetCurrentSceneIndex();
+        if (currentIndex >= 0 && currentIndex < _sceneAddresses.Count - 1)
+        {
+            await LoadSceneByAddress(_sceneAddresses[currentIndex + 1]);
+        }
+    }
+    
+    public async void LoadPreviousScene()
+    {
+        int currentIndex = GetCurrentSceneIndex();
+        if (currentIndex > 0)
+        {
+            await LoadSceneByAddress(_sceneAddresses[currentIndex - 1]);
+        }
+    }
+    
+    public async void ReloadScene()
+    {
+        if (_currentSceneRef != null && _currentSceneRef.RuntimeKeyIsValid())
+        {
+            await LoadSceneByAddress(_currentSceneRef.AssetGUID);
+        }
+    }
+    
+    private async Awaitable LoadSceneByAddress(string sceneAddress)
+    {
+        if (_currentSceneHandle.IsValid())
+        {
+            Addressables.Release(_currentSceneHandle);
+        }
+        
+        _currentSceneHandle = Addressables.LoadSceneAsync(sceneAddress, activateOnLoad: true);
+        
+        await _currentSceneHandle.Task;
+        
+        UpdateCurrentSceneReference(sceneAddress);
+    }
+    
+    private int GetCurrentSceneIndex()
+    {
+        if (_currentSceneRef != null && _currentSceneRef.RuntimeKeyIsValid())
+        {
+            string currentGuid = _currentSceneRef.AssetGUID;
+            return _sceneAddresses.IndexOf(currentGuid);
+        }
+        return -1;
+    }
+    
+    private void UpdateCurrentSceneReference(string sceneAddress)
+    {
+        foreach (var sceneRef in _sceneReferences)
+        {
+            if (sceneRef.AssetGUID == sceneAddress)
+            {
+                _currentSceneRef = sceneRef;
+                return;
+            }
+        }
+        Debug.LogWarning($"Scene address {sceneAddress} not found in references!");
     }
 }
